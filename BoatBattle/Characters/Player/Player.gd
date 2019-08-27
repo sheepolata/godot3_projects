79cc1645b428bd7a8d0c4efa0e92ec1a09d82b4b
sheepolata <antoine.gaget@gmail.com>
2019-08-27 14:32:11 +0100

    Add water shaders

--- BoatBattle/Characters/Player/Player.gd
+++ BoatBattle/Characters/Player/Player.gd
@@ -5,6 +5,8 @@ var current_speeds : Vector2 = Vector2.ZERO
 
 export(int) var SPEED = 550
 export(float, 0, 50) var SPEED_INCREMENT = 4
+var current_speed_increment : float = 0
+export var speed_increment_factor : float = 1.01
 
 export(int, 0, 90) var TURN_SPEED = 90
 export(float, 0, 1) var TURN_SPEED_INCREMENT = 3
@@ -16,16 +18,23 @@ var debug_dict : Dictionary = {}
 var collision_info : KinematicCollision2D = null
 
 onready var cam = $Camera2D
-var max_zoom_in = 0.6; var max_zoom_out = 2;
+var max_zoom_in = 0.9; var max_zoom_out = 1.8;
+
+onready var water_effect = $WaterEffect
+
+onready var anim = $AnimationPlayer
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	add_to_group("player")
 	set_physics_process(true)
+	
+	water_effect.start()
+	#anim.play("bg_anim")
 
 func _physics_process(delta : float):
 	update()
-	
+
 	var v = Utils.normalise(abs(current_speeds.y), max_zoom_in, max_zoom_out, 0, SPEED)
 	cam.zoom = Vector2(v, v)
 	
@@ -50,22 +59,33 @@ func movement_loop(delta : float):
 		current_speeds.x = max(current_speeds.x + move_direction.x * TURN_SPEED_INCREMENT, -TURN_SPEED)
 	else:
 		if current_speeds.x > 0:
-			current_speeds.x = max(0, current_speeds.x - TURN_SPEED_INCREMENT*2)
+			current_speeds.x = max(0, current_speeds.x - TURN_SPEED_INCREMENT)
 		else:
-			current_speeds.x = min(0, current_speeds.x + TURN_SPEED_INCREMENT*2)
+			current_speeds.x = min(0, current_speeds.x + TURN_SPEED_INCREMENT)
 	
 	if move_direction.y > 0:
-		current_speeds.y = min(current_speeds.y + move_direction.y * SPEED_INCREMENT, SPEED)
-	elif move_direction.y < 0:
-		current_speeds.y = max(current_speeds.y + move_direction.y * SPEED_INCREMENT, -SPEED)
-	else:
-		if current_speeds.y > 0:
-			current_speeds.y = max(0, current_speeds.y - SPEED_INCREMENT*4)
+		if current_speed_increment == 0:
+			current_speed_increment = 0.1
 		else:
-			current_speeds.y = min(0, current_speeds.y + SPEED_INCREMENT*4)
+			current_speed_increment = max(SPEED_INCREMENT, current_speed_increment * speed_increment_factor)
+			
+		current_speeds.y = min(current_speeds.y + move_direction.y * current_speed_increment, SPEED)
+	elif move_direction.y < 0:
+		if current_speed_increment == 0:
+			current_speed_increment = 0.1
+		else:
+			current_speed_increment = max(SPEED_INCREMENT, current_speed_increment * speed_increment_factor)
+			
+		current_speeds.y = max(current_speeds.y + move_direction.y * current_speed_increment, -SPEED)
+	else:
+		current_speed_increment = 0
+		if current_speeds.y > 0:
+			current_speeds.y = max(0, current_speeds.y - SPEED_INCREMENT*2)
+		else:
+			current_speeds.y = min(0, current_speeds.y + SPEED_INCREMENT*2)
 	
 	rotation_degrees += current_speeds.x * delta
-	print(current_speeds)
+	#print(current_speeds)
 	
 	collision_info = move_and_collide(Vector2(current_speeds.y * cos(rotation), current_speeds.y * sin(rotation)) * delta)
 
@@ -83,7 +103,18 @@ func controls_loop():
 	move_direction.x = -int(LEFT) + int(RIGHT)
 	move_direction.y = -int(DOWN) + int(UP)
 
+
+func _on_WaterEffect_timeout():
+	var this_effect = preload("res://Engine/WaveEffect.tscn").instance()
+	this_effect.min_scale = 0
+	this_effect.max_scale = Utils.normalise(abs(current_speeds.y), 2, this_effect._MAX_SCALE, 0, SPEED)
 	
+	water_effect.wait_time = Utils.normalise(SPEED - abs(current_speeds.y), 0.1, 0.35, 0, SPEED)
+	
+	get_parent().add_child(this_effect)
+	
+	this_effect.position = to_global($WaveEffectSpawnPoint.position)
+
 	
 	
 	
---
+++ BoatBattle/Engine/WaveEffect.gd
@@ -0,0 +1,27 @@
+extends Sprite
+
+export(float) var duration : float = 0.5
+const _MAX_SCALE : float = 5.0;
+var min_scale : float = 0 setget set_min_scale; 
+var max_scale : float = 3 setget set_max_scale;
+
+onready var tween = $Tween
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	scale = Vector2(min_scale, min_scale)
+	tween.interpolate_property(self, "scale", Vector2(min_scale, min_scale), Vector2(max_scale, max_scale), duration, Tween.TRANS_LINEAR, Tween.EASE_IN_OUT)
+	tween.start()
+	
+func set_max_scale(value):
+	max_scale = value
+	#tween.interpolate_property(self, "scale", Vector2(min_scale, min_scale), Vector2(max_scale, max_scale), duration, Tween.TRANS_LINEAR, Tween.EASE_IN_OUT)
+	#tween.start()
+	
+func set_min_scale(value):
+	min_scale = value
+	#tween.interpolate_property(self, "scale", Vector2(min_scale, min_scale), Vector2(max_scale, max_scale), duration, Tween.TRANS_LINEAR, Tween.EASE_IN_OUT)
+	#tween.start()
+	
+func _on_Tween_tween_all_completed():
+	queue_free()
