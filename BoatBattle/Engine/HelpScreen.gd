349f966fc398a2f306f9b10a74887ff5a4893b56
sheepolata <antoine.gaget@gmail.com>
2019-09-03 13:56:56 +0100

    Add help/start screen

---
+++ BoatBattle/Engine/HelpScreen.gd
@@ -0,0 +1,7 @@
+extends Node2D
+
+func _process(delta):
+	if Input.is_action_just_pressed("ui_cancel"):
+		get_tree().quit()
+	if Input.is_action_just_pressed("ui_accept"):
+		get_tree().change_scene("res://Environments/World.tscn")
